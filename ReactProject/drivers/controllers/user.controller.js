1643d69670ba9da812280bf08715e432088b1f7d
BiPi2016 <bp.singh@hotmail.com>
2020-06-30 12:37:32 +0200

    signin process started, user can be seached and matched

--- ReactProject/drivers/controllers/user.controller.js
+++ ReactProject/drivers/controllers/user.controller.js
@@ -9,10 +9,50 @@ const {
     validationResult
 } = require("express-validator");
 
-exports.postSignIn = async (req, res, next) => {
-    try {
-        res.json('Signin in');
-    } catch (error) {
-        next(error);
+exports.postSignIn = [
+    check('email').isEmail().withMessage("Provide a valid email"),
+    check("password")
+    .not()
+    .isEmpty()
+    .withMessage("You must provide a password")
+    .isLength({
+        min: 6,
+    })
+    .withMessage("Password must have 6 or more characters")
+    .escape()
+    .trim(),
+    async (req, res, next) => {
+        try {
+            //Validation results
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(422).json({
+                    errors: errors.array()
+                });
+            }
+
+            //Fetching the user
+            const {
+                email,
+                password
+            } = req.body;
+            const user = await User.findOne({
+                email: email
+            });
+            if (user || user !== null) {
+                const isAuthenticUser = await comparePasswords(password, user.password);
+                if (isAuthenticUser) { // User found and password matches the hash
+                    return res.json('Authentication ' + isAuthenticUser);
+                }
+            }
+            //No registered email or matching password
+            return res.status(401).json({
+                errors: [{
+                    msg: 'Authenication failed, wrond email or password'
+                }]
+            });
+        } catch (error) {
+            next(error);
+        }
     }
-};
+];
---
+++ ReactProject/drivers/util/validators.js
@@ -0,0 +1,6 @@
+const {
+    check,
+    validationResult
+  } = require("express-validator");
+
+  exports.checkName = (req, body)
